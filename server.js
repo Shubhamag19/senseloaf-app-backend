9e0433f1040d426b79160116a47c0abd4ed382af
Shubham Agrawal <shubhamagrawal154@gmail.com>
2025-02-07 19:18:13 +0530

    update env

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+
+const PORT = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: PORT });
 
 const questionSets = require("./data");
 
